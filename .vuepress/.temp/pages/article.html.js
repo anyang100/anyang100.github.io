14a9d11a62a89be45583ac5eabb573f061fd7a15
NGKINCHUNG <archaeology.cn@outlook.com>
2022-08-17 07:41:01 +0800

    minor changes

--- .vuepress/.temp/pages/article.html.js
+++ .vuepress/.temp/pages/article.html.js
@@ -1,14 +1,1 @@
-export const data = JSON.parse("{\"key\":\"v-0989631c\",\"path\":\"/article.html\",\"title\":\"論文\",\"lang\":\"zh-TW\",\"frontmatter\":{\"lang\":\"zh-TW\",\"title\":\"論文\",\"author\":\"NG, Kinchung\",\"date\":\"2022-01-22T00:00:00.000Z\",\"summary\":\"論文 1. 張蔚然：〈殷墟地層研究〉，載李濟總編輯：《安陽發掘報告（第二期）》，中央研究所歷史語言研究所，1930年，第253-285頁。 2. 李濟：〈小屯與仰韶〉，載李濟總編輯：《安陽發掘報告（第二期）》，中央研究所歷史語言研究所，1930年，第337-347頁。 3. 李濟：〈現代考古學與殷墟發掘〉，載李濟總編輯：《安陽發掘報告（第二期）》，中央研究所\",\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"https://ay1928.github.io/article.html\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"安陽・一九二八\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"論文\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"article\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"zh-TW\"}],[\"meta\",{\"property\":\"article:author\",\"content\":\"NG, Kinchung\"}],[\"meta\",{\"property\":\"article:published_time\",\"content\":\"2022-01-22T00:00:00.000Z\"}]]},\"excerpt\":\"\",\"headers\":[],\"readingTime\":{\"minutes\":0.91,\"words\":273},\"filePathRelative\":\"article.md\",\"localizedDate\":\"2022年1月22日\"}")
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
+export const data = JSON.parse("{\"key\":\"v-0989631c\",\"path\":\"/article.html\",\"title\":\"論文\",\"lang\":\"zh-TW\",\"frontmatter\":{\"lang\":\"zh-TW\",\"title\":\"論文\",\"author\":\"NG, Kinchung\",\"date\":\"2022-01-22T00:00:00.000Z\",\"summary\":\"論文 1. 張蔚然：〈殷墟地層研究〉，載李濟總編輯：《安陽發掘報告（第二期）》，中央研究所歷史語言研究所，1930年，第253-285頁。 2. 李濟：〈小屯與仰韶〉，載李濟總編輯：《安陽發掘報告（第二期）》，中央研究所歷史語言研究所，1930年，第337-347頁。 3. 李濟：〈現代考古學與殷墟發掘〉，載李濟總編輯：《安陽發掘報告（第二期）》，中央研究所\",\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"https://ay1928.github.io/article.html\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"安陽・一九二八\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"論文\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"article\"}],[\"meta\",{\"property\":\"og:updated_time\",\"content\":\"2022-08-10T09:56:15.000Z\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"zh-TW\"}],[\"meta\",{\"property\":\"article:author\",\"content\":\"NG, Kinchung\"}],[\"meta\",{\"property\":\"article:published_time\",\"content\":\"2022-01-22T00:00:00.000Z\"}],[\"meta\",{\"property\":\"article:modified_time\",\"content\":\"2022-08-10T09:56:15.000Z\"}]]},\"excerpt\":\"\",\"headers\":[],\"git\":{\"createdTime\":1659965549000,\"updatedTime\":1660125375000,\"contributors\":[{\"name\":\"NGKINCHUNG\",\"email\":\"archaeology.cn@outlook.com\",\"commits\":2}]},\"readingTime\":{\"minutes\":0.91,\"words\":273},\"filePathRelative\":\"article.md\",\"localizedDate\":\"2022年1月22日\"}")
