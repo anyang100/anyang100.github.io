203c5f3301963f3fa969772a561ece12efeeea14
citenote <archaeology.cn@outlook.com>
2023-02-24 16:10:51 +0800

    update about page

--- docs/.vuepress/.temp/pages/about.html.vue
+++ docs/.vuepress/.temp/pages/about.html.vue
@@ -1,5 +1,9 @@
 <template><div><h1 id="關於" tabindex="-1"><a class="header-anchor" href="#關於" aria-hidden="true">#</a> 關於</h1>
-<p>1928年10月13日，董作賓領隊在安陽小屯的田野挖下第一鏟，拉開了殷墟考古的序幕。</p>
+<p>這是一個興趣使然的課餘項目，意在搜集和整理河南安陽考古學研究的文獻，並製作成資料索引集，予芸芸學子使用。</p>
+<p>中文文獻參考《考古學報》的註釋格式。英文文獻統一使用APA格式。</p>
+<p>資料主要來源：<a href="https://www.cnki.net/" target="_blank" rel="noopener noreferrer">中國知網<ExternalLinkIcon/></a></p>
+<p>北京大學考古文博學院考古系在讀博士生阿聰</p>
+<p>如有任何想法，歡迎電郵至ngkinchung@stu.pku.edu.cn。</p>
 </div></template>
 
 
