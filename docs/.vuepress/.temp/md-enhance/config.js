1f569f7bdbb3b97edb2231411cd592cfe4b8c0be
citenote <archaeology.cn@outlook.com>
2023-06-05 13:39:18 +0800

    modify script

--- docs/.vuepress/.temp/md-enhance/config.js
+++ docs/.vuepress/.temp/md-enhance/config.js
@@ -6,4 +6,7 @@ export default defineClientConfig({
   enhance: ({ app }) => {
 
   },
+  setup: () => {
+
+  }
 });
---
+++ docs/.vuepress/dist/assets/reading-the-past-current-approaches-to-interpretation-in-archaeology.html-1be6b197.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./plugin-vue_export-helper-c27b6911.js";import{o as r,c as t}from"./app-42fd6027.js";const o={};function a(n,c){return r(),t("div")}const p=e(o,[["render",a],["__file","reading-the-past-current-approaches-to-interpretation-in-archaeology.html.vue"]]);export{p as default};
